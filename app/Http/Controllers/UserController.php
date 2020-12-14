ad5bad2953bd304a17bea41d2e53567718c2f44b
Mihai Georgevici <mihai@titlecapture.com>
2020-12-14 21:50:45 +0200

    Cleanup

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,6 +12,7 @@ class UserController extends Controller
 {
         public function __construct() {
                 $this->middleware('auth:api', ['except' => ['show']]);
+                $this->middleware('is_admin:api', ['only' => ['']]);
         }
 
 
--- routes/api.php
+++ routes/api.php
@@ -22,12 +22,6 @@ Route::post('auth/register',        'AuthController@register');
 Route::post('auth/refresh-token',   'AuthController@refreshToken');
 Route::post('auth/logout',          'AuthController@logout');
 
-// Admin Auth:
-Route::get('admin/auth/admin-profile',    'AdminAuthController@adminProfile');
-Route::post('admin/auth/login',           'AdminAuthController@login');
-Route::post('admin/auth/register',        'AdminAuthController@register');
-Route::post('admin/auth/refresh-token',   'AdminAuthController@refreshToken');
-Route::post('admin/auth/logout',          'AdminAuthController@logout');
 
 // Users:
 Route::get('users',                 'UserController@index');
